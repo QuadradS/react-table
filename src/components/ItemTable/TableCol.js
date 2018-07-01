c6ad276a3ca447b0ca011a23cf3a1a8bf35937f6
Ситдиков <Quadrad4@gmail.com>
2018-07-01 19:55:41 +0600

    - Ready a DragAndDrop and Resize of column

--- src/components/ItemTable/TableCol.js
+++ src/components/ItemTable/TableCol.js
@@ -1,42 +1,73 @@
 import React from 'react';
 import Check from './../ui/icons/Check';
+import {Resizable, ResizableBox} from 'react-resizable';
 
-const TableCol = (props) => {
+class TableCol extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: 260,
+            drag: false
+        }
+    }
 
-    return(
-            <div className="table-col">
+    onResize = (event, {element, size}) => {
+        this.setState({width: size.width, height: size.height});
+    };
+
+    render() {
+        return (
+            <div className="box">
                 <div className="table-row--cell table-row--cell-header">
-                    <p>{props.title}</p>
+                    <p>{this.props.title}</p>
                 </div>
-                <div className="table-row--cell table-row--cell-search">
-                    <input type="text" placeholder="Search..."/>
+                <div className="no-drop">
+                    <div className="table-row--cell table-row--cell-search">
+                        <input type="text" placeholder="Search..."/>
+                    </div>
+                    <ResizableBox className="box " width={this.state.width} axis="x" contenteditable="true">
+                        {
+                            this.props.items.map((item, key) => (
+                                <div key={key} className="table-row--cell table-row--cell-info" contenteditable="false">
+                                    {item}
+                                </div>
+                            ))
+                        }
+                    </ResizableBox>
                 </div>
-                {
-                    props.items.map((item)=>(
-                        <div className="table-row--cell table-row--cell-info">
-                            {item}
-                        </div>
-                    ))
-                }
-
             </div>
+        )
+    }
 
-    )
 };
 
 export default TableCol;
 
-{/*<div className="table-col">*/}
-    {/*<div className="table-row--cell table-row--cell-header">*/}
-        {/*<div className="check-box check-box-on">*/}
-            {/*<Check className="check-box--check check-box--check-on"/>*/}
-        {/*</div>*/}
-        {/*<p>{item}</p>*/}
-    {/*</div>*/}
-    {/*<div className="table-row--cell table-row--cell-search">*/}
-        {/*<input type="text" placeholder="Search..."/>*/}
-    {/*</div>*/}
-    {/*<div className="table-row--cell table-row--cell-info">*/}
-        {/*USA*/}
-    {/*</div>*/}
-{/*</div>*/}
+{/*<div className="table-col">*/
+}
+{/*<div className="table-row--cell table-row--cell-header">*/
+}
+{/*<div className="check-box check-box-on">*/
+}
+{/*<Check className="check-box--check check-box--check-on"/>*/
+}
+{/*</div>*/
+}
+{/*<p>{item}</p>*/
+}
+{/*</div>*/
+}
+{/*<div className="table-row--cell table-row--cell-search">*/
+}
+{/*<input type="text" placeholder="Search..."/>*/
+}
+{/*</div>*/
+}
+{/*<div className="table-row--cell table-row--cell-info">*/
+}
+{/*USA*/
+}
+{/*</div>*/
+}
+{/*</div>*/
+}
